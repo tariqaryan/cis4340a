106ed4b87ca89134c4f7e1d876a33726e1e536ae
tariqaryan <101847729+tariqaryan@users.noreply.github.com>
2025-03-02 16:55:45 -0500

    Update R09 LCK05 J.java

--- R09 LCK05 J.java
+++ R09 LCK05 J.java
@@ -1,8 +1,11 @@
-/* This class is not thread-safe */
+/* This class is thread-safe */
 public final class CountHits {
   private static int counter;
+  private static final Object lock = new Object();
  
   public void incrementCounter() {
-    counter++;
+    synchronized (lock) {
+      counter++;
+    }
   }
 }
