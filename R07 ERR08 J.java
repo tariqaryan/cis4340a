e331519ad2658482665a616708a2aedc6ff6c9ae
tariqaryan <101847729+tariqaryan@users.noreply.github.com>
2025-03-02 16:49:11 -0500

    Update R07 ERR08 J.java

--- R07 ERR08 J.java
+++ R07 ERR08 J.java
@@ -1,12 +1,11 @@
+
 boolean isName(String s) {
-  try {
-    String names[] = s.split(" ");
- 
-    if (names.length != 2) {
-      return false;
-    }
-    return (isCapitalized(names[0]) && isCapitalized(names[1]));
-  } catch (NullPointerException e) {
+  if (s == null) {
     return false;
   }
+  String names[] = s.split(" ");
+  if (names.length != 2) {
+    return false;
+  }
+  return (isCapitalized(names[0]) && isCapitalized(names[1]));
 }
